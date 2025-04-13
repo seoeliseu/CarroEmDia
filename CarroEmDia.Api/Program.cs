f837aef466ac5f304f09ff45301efc9e13442a77
Eliseu Mendes de Morais <seoeliseu@gmail.com>
2025-04-13 17:42:12 -0300

    jwt add

--- CarroEmDia.Api/Program.cs
+++ CarroEmDia.Api/Program.cs
@@ -1,17 +1,36 @@
+using System.Text;
 using CarroEmDia.Startup;
 using CarroEmDia.Api.Endpoints;
 using CarroEmDia.Api.Middlewares;
+using Microsoft.IdentityModel.Tokens;
 using CarroEmDia.Application.Shared.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddServices(builder.Configuration);
+builder.Services.Configure<ApiKeySettings>(builder.Configuration.GetSection("ApiKeySettings"));
+builder.Services.AddAuthorization();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var secretKey = builder.Configuration["JwtSettings:SecretKey"];
+        if (string.IsNullOrEmpty(secretKey))
+        {
+            throw new InvalidOperationException("JwtSettings:SecretKey is not configured.");
+        }
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+        };
+    });
 
 var app = builder.Build();
-
-builder.Services.Configure<ApiKeySettings>(
-    builder.Configuration.GetSection("ApiKeySettings")
-);
 
 app.UseMiddleware<AuthenticationMiddleware>();
 
